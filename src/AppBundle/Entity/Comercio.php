39e9a2271e20b2939b496249930a466fafef403a
Carlos Salvatierra <cslucano@gmail.com>
2015-05-17 05:53:19 -0500

    Added Comercio Entity

---
+++ src/AppBundle/Entity/Comercio.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Comercio
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\ComercioRepository")
+ */
+class Comercio
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre_comercial", type="string", length=255)
+     */
+    private $nombreComercial;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre_via", type="string", length=255)
+     */
+    private $nombreVia;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="numero_via", type="string", length=255)
+     */
+    private $numeroVia;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="giro", type="string", length=255)
+     */
+    private $giro;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="lon", type="float")
+     */
+    private $lon;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="lat", type="float")
+     */
+    private $lat;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombreComercial
+     *
+     * @param string $nombreComercial
+     *
+     * @return Comercio
+     */
+    public function setNombreComercial($nombreComercial)
+    {
+        $this->nombreComercial = $nombreComercial;
+
+        return $this;
+    }
+
+    /**
+     * Get nombreComercial
+     *
+     * @return string
+     */
+    public function getNombreComercial()
+    {
+        return $this->nombreComercial;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Comercio
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set nombreVia
+     *
+     * @param string $nombreVia
+     *
+     * @return Comercio
+     */
+    public function setNombreVia($nombreVia)
+    {
+        $this->nombreVia = $nombreVia;
+
+        return $this;
+    }
+
+    /**
+     * Get nombreVia
+     *
+     * @return string
+     */
+    public function getNombreVia()
+    {
+        return $this->nombreVia;
+    }
+
+    /**
+     * Set numeroVia
+     *
+     * @param string $numeroVia
+     *
+     * @return Comercio
+     */
+    public function setNumeroVia($numeroVia)
+    {
+        $this->numeroVia = $numeroVia;
+
+        return $this;
+    }
+
+    /**
+     * Get numeroVia
+     *
+     * @return string
+     */
+    public function getNumeroVia()
+    {
+        return $this->numeroVia;
+    }
+
+    /**
+     * Set giro
+     *
+     * @param string $giro
+     *
+     * @return Comercio
+     */
+    public function setGiro($giro)
+    {
+        $this->giro = $giro;
+
+        return $this;
+    }
+
+    /**
+     * Get giro
+     *
+     * @return string
+     */
+    public function getGiro()
+    {
+        return $this->giro;
+    }
+
+    /**
+     * Set lon
+     *
+     * @param float $lon
+     *
+     * @return Comercio
+     */
+    public function setLon($lon)
+    {
+        $this->lon = $lon;
+
+        return $this;
+    }
+
+    /**
+     * Get lon
+     *
+     * @return float
+     */
+    public function getLon()
+    {
+        return $this->lon;
+    }
+
+    /**
+     * Set lat
+     *
+     * @param float $lat
+     *
+     * @return Comercio
+     */
+    public function setLat($lat)
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    /**
+     * Get lat
+     *
+     * @return float
+     */
+    public function getLat()
+    {
+        return $this->lat;
+    }
+}
+
