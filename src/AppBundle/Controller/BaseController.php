8cc718fddf547b67ee5b530b760872967d63f9c5
Carlos Salvatierra <cslucano@gmail.com>
2015-05-16 23:43:15 -0500

    Primer commit

---
+++ src/AppBundle/Controller/BaseController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use JMS\Serializer\Serializer;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+abstract class BaseController extends Controller
+{
+    function serialize($object, $format = 'json')
+    {
+        /** @var Serializer $serializer */
+        $serializer = $this->get('jms_serializer');
+        return $serializer->serialize($object, $format);
+    }
+}
